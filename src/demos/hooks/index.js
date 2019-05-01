7f79b23e7ba59a64a9f2e756bab0d3a2343d9ff9
YanPanMichael <yanpancsu@icloud.com>
2019-05-02 00:25:29 +0800

    hooks useEffect update

--- src/demos/hooks/index.js
+++ src/demos/hooks/index.js
@@ -1,7 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default () => {
   const [name, setName] = useState("Michael");
+  useEffect(() => {
+    console.log('function component update');
+  });
   return (
     <>
       <p>My name is: {name}</p>
