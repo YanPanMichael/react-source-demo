b32381e3811b5fde0240cca174dafad506618e64
YanPanMichael <yanpancsu@icloud.com>
2019-05-01 23:29:59 +0800

    react children demo

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
-import React, { Component } from 'react';
-import RefDemo from './demos/ref/index';
-import ComponentDemo from './demos/forward-ref/index';
-import ContextDemo from './demos/context/index';
-import './App.css';
+import React, { Component } from "react";
+import RefDemo from "./demos/ref/index";
+import ComponentDemo from "./demos/forward-ref/index";
+import ContextDemo from "./demos/context/index";
+import ChildrenDemo from "./demos/children/index";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <h3>createRef</h3>
           <p>
-            <h3>createRef</h3>
             <RefDemo />
           </p>
 
+          <h3>ForwardRef</h3>
           <p>
-            <h3>ForwardRef</h3>
             <ComponentDemo />
           </p>
 
+          <h3>ContextRef</h3>
           <p>
-            <h3>ContextRef</h3>
             <ContextDemo />
+          </p>
+
+          <h3>ChildrenDemo</h3>
+          <p>
+            <ChildrenDemo />
           </p>
         </header>
       </div>
