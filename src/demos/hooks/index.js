ab19cb04c21c5f189b5acabb57d9157ef13e2d53
YanPanMichael <yanpancsu@icloud.com>
2019-10-09 23:44:34 +0800

    increase hooks

--- src/demos/hooks/index.js
+++ src/demos/hooks/index.js
@@ -2,13 +2,22 @@ import React, { useState, useEffect } from "react";
 
 export default () => {
   const [name, setName] = useState("Michael");
+  const [age, setAge] = useState(16);
   useEffect(() => {
-    console.log('function component update');
+    console.log('function component name update');
 
     return () => {
-      console.log('unMount');
+      console.log('function component name unMount');
     }
-  }, []);
+  }, [name]);
+
+  useEffect(() => {
+    console.log('function component age update');
+
+    return () => {
+      console.log('function component age unMount');
+    }
+  }, [age]);
   return (
     <>
       <p>My name is: {name}</p>
