3bc6d834e2a58d211186d3f4944df4dfa70127b5
YanPanMichael <yanpancsu@icloud.com>
2019-10-12 00:02:27 +0800

    suspense demo

--- src/demos/suspense/App.js
+++ src/demos/suspense/App.js
@@ -3,7 +3,30 @@
 import React from "react";
 import StockTable from "./StockTable";
 
-import StockChart from "./StockChart";
+// import StockChart from "./StockChart";
+
+// const StockChart = React.lazy(() =>
+//   import(/* webpackChunkName: 'StockChart' */ "./StockChart")
+// );
+
+const StockChart = React.lazy(() =>
+  new Promise(resolve => setTimeout(resolve, 3000))
+    .then(() => import(/* webpackChunkName: 'StockChart' */ "./StockChart"))
+    .catch(err => {
+      console.log(err);
+    })
+);
+
+const StockChart2 = React.lazy(() =>
+  Promise.all([
+    import(/* webpackChunkName: 'StockChart' */ "./StockChart"),
+    new Promise(resolve => setTimeout(() => resolve(), 3000))
+  ])
+    .then(([modulesExports]) => modulesExports)
+    .catch(err => {
+      console.log(err);
+    })
+);
 
 class App extends React.Component {
   state = {
@@ -14,16 +37,18 @@ class App extends React.Component {
     const { selectedStock } = this.state;
     return (
       <React.Fragment>
-        <StockTable
-          stocks={stocks}
-          onSelect={selectedStock => this.setState({ selectedStock })}
-        />
-        {selectedStock && (
-          <StockChart
-            stock={selectedStock}
-            onClose={() => this.setState({ selectedStock: false })}
+        <React.Suspense fallback={<div>Loading...</div>}>
+          <StockTable
+            stocks={stocks}
+            onSelect={selectedStock => this.setState({ selectedStock })}
           />
-        )}
+          {selectedStock && (
+            <StockChart
+              stock={selectedStock}
+              onClose={() => this.setState({ selectedStock: false })}
+            />
+          )}
+        </React.Suspense>
       </React.Fragment>
     );
   }
